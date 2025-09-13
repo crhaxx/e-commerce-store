32fc1d9d822d11620beffa9d9cd1ced5d95a0d41
developer-sogyy <opekanyhermelin@gmail.com>
2025-09-13 12:43:12 +0200

    Finished logout

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Allow to accept JSON data in request body
+app.use(cookieParser()); // Allow to parse cookies from request
 
 app.use("/api/auth", authRoutes);
 
