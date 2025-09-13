7e9e2a16d93c7d6800fdafe4d533833c688fe36c
developer-sogyy <opekanyhermelin@gmail.com>
2025-09-13 17:33:35 +0200

    Added coupons

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 dotenv.config();
 
@@ -19,6 +21,7 @@ app.use(cookieParser()); // Allow to parse cookies from request
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
