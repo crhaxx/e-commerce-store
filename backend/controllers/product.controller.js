eb76a83d4e1add3f0098d6072e467974dd631de7
developer-sogyy <opekanyhermelin@gmail.com>
2025-09-13 15:44:28 +0200

    Added GetProducts by category, products and recommended && toggleFeatured on product

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -12,6 +12,17 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
+export const getProductsByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const products = await Product.find({ category });
+    res.json(products);
+  } catch (error) {
+    console.log("Error fetching products by category:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
 export const getFeaturedProducts = async (req, res) => {
   try {
     let featuredProducts = await redis.get("featuredProducts");
@@ -33,6 +44,28 @@ export const getFeaturedProducts = async (req, res) => {
     res.json(featuredProducts);
   } catch (error) {
     console.log("Error fetching featured products:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      { $sample: { size: 3 } },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]); // Get 3 random products
+
+    res.json(products);
+  } catch (error) {
+    console.log("Error fetching recommended products:", error);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
@@ -93,3 +126,32 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updatedProduct = await product.save();
+
+      // Update Redis cache
+      await updateFeaturedProductsCache();
+
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.log("Error toggling featured status:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+async function updateFeaturedProductsCache() {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean(); // .lean() to get plain JS objects - better performance
+    await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+  } catch (error) {
+    console.log("Error updating featured products cache:", error);
+  }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,16 +2,22 @@ import express from "express";
 import {
   createProduct,
   deleteProduct,
+  toggleFeaturedProduct,
   getAllProducts,
+  getProductsByCategory,
   getFeaturedProducts,
+  getRecommendedProducts,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/featured", getFeaturedProducts);
+router.get("/recomendations", protectRoute, getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
